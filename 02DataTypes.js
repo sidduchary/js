fada9bebcbfebe5652cbf750292e21752a2928ea
sidduchary <siddivinayak256@gmail.com>
2025-08-03 16:43:15 +0000

    datatypes

---
+++ 02DataTypes.js
@@ -0,0 +1,22 @@
+"use strict" // treats all code as newer js version
+
+// alert(3+3) // works only in browser, not in node
+
+console.log("hii")
+console.log("improve readability")
+
+let name= "Vexx"
+let age= 20     //number range 2 power 50-60s
+let hasAccount= true
+
+// bigint for large numbers
+//string "" use double codes
+// boolean  true/false
+// null ->standalone value
+// symbol  -> unique
+
+// object
+
+console.log(typeof null)   // -> object
+console.log(typeof undefined)   // -> undefined
+console.log(typeof name)    // -> string
